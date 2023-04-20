1689ab8e86b42e81178d6a566a550bb50ed88b20
s <96222718+sa6ds@users.noreply.github.com>
2023-04-19 22:13:36 -0400

    made auth pages responsive

--- src/pages/auth/password.tsx
+++ src/pages/auth/password.tsx
@@ -3,17 +3,23 @@ import Link from "next/link";
 
 const PasswordPage: NextPage = () => {
   return (
-    <div className="flex text-base font-light">
+    <div className="font-light md:flex">
       {/* LEFT SIDE */}
-      <div className="mx-auto my-auto w-full min-w-min max-w-sm py-4">
+      <div className="mx-auto my-auto w-full py-4 md:w-[500px]">
         {/* FORM */}
-        <form className="ml-32 mt-24 border-stone-800">
+        <form className="mt-24 flex w-full flex-col border-stone-800 px-12 md:px-6">
           <h1 className="text-3xl tracking-wide">Forgot your password?</h1>
-          <p className="mt-2 pb-5 text-lg">Get back your account.</p>
-          <p className="py-2 font-[500]">Email</p>
-          <input className="w-96 rounded-lg border border-gray-300 bg-gray-100 px-3 py-1.5"></input>
+          <p className="pb-5 pt-2 text-lg">
+            Remember your password?{" "}
+            <Link className="text-blue-500" href={"/auth/login"}>
+              Sign in.
+            </Link>
+          </p>
 
-          <button className="duration-1500 mt-10 w-96 rounded-[10px] border border-black bg-black px-7 py-1.5 text-white transition-all hover:bg-white hover:text-black">
+          <p className="py-2 font-[500]">Email</p>
+          <input className="w-full rounded-lg border border-gray-300 bg-gray-100 px-3 py-1.5"></input>
+
+          <button className="duration-1500 mt-10 w-full rounded-[10px] border border-black bg-black px-7 py-1.5 text-white transition-all hover:bg-white hover:text-black">
             Send Password Reset Link
           </button>
         </form>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,35 @@
 import { type NextPage } from "next";
-import Link from "next/link";
 import Image from "next/image";
-import placeholderImg from "../../public/placeholder_img.png";
+import placeholderImg from "../assets/images/placeholder_img.png";
 import Footer from "~/Components/Footer";
 import Navbar from "~/Components/Navbar";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="container mx-auto p-6 pb-48 font-light">
+      <div className="container mx-auto max-w-7xl p-6 pb-48 font-light">
         <Navbar />
+        <pre>
+          <a href="content/dashboard">secret door</a>
+        </pre>
 
-        <a href="content/dashboard">dashboard</a>
-        <div className="flex">
-          <div className="flex flex-col justify-center">
-            <h1 className="pb-2 pt-64 text-5xl">Reselling Simplified</h1>
-            <p className="w-[650px] pt-4 text-2xl">
+        <div className="mt-12 inline text-center lg:mt-28 lg:flex lg:text-left">
+          <div className="mb-12 mt-12 flex flex-col lg:mt-48 lg:flex lg:text-left">
+            <h1 className="mb-2 mt-12 text-4xl lg:mt-36 lg:text-5xl">
+              Reselling Simplified
+            </h1>
+            <p className="mt-4 text-xl md:text-2xl lg:w-[650px]">
               Elevate your reselling game with advanced tools, inventory
               tracking, and more.
             </p>
           </div>
           <Image
-            className="ml-auto w-96"
+            className="mx-auto w-72 md:w-96 lg:mx-0 lg:ml-auto lg:w-96"
             src={placeholderImg}
             alt="App Preview"
           />
         </div>
       </div>
-
       <Footer />
     </div>
   );
